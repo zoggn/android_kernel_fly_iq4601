b46cdb9fba4521b3ee514c5b7c8eb889ee49b82d
zoggn <kurkov.nikita@gmail.com>
2018-05-10 22:13:46 +0300

    update cust for leds

--- arch/arm/mach-mt6572/vanzo6572_wet_l/leds/mt65xx/cust_leds.c
+++ arch/arm/mach-mt6572/vanzo6572_wet_l/leds/mt65xx/cust_leds.c
@@ -46,7 +46,7 @@ static struct cust_mt65xx_led cust_led_list[MT65XX_LED_TYPE_TOTAL] = {
 	{"jogball-backlight", MT65XX_LED_MODE_NONE, -1, {0}},
 	{"keyboard-backlight",MT65XX_LED_MODE_NONE, -1, {0}},
 	{"button-backlight",  MT65XX_LED_MODE_NONE, -1, {0}},
-	{"lcd-backlight",     MT65XX_LED_MODE_PMIC, MT65XX_LED_PMIC_LCD_ISINK, {0}},
+	{ "lcd-backlight",    MT65XX_LED_MODE_PWM,   0, { 1, 1, 3, 3, 0 }};
 };
 
 struct cust_mt65xx_led *get_cust_led_list(void)
