cff0d2f5cfbd77cf4bd41e8bfd59c3737e711c3c
zoggn <kurkov.nikita@gmail.com>
2018-05-16 19:51:18 +0300

    set correct dirrection for accelerometr

--- arch/arm/mach-mt6572/vanzo6572_wet_l/accelerometer/KXTJ2_1009/cust_acc.c
+++ arch/arm/mach-mt6572/vanzo6572_wet_l/accelerometer/KXTJ2_1009/cust_acc.c
@@ -15,7 +15,7 @@ int cust_acc_power(struct acc_hw *hw, unsigned int on, char* devname)
 /*---------------------------------------------------------------------------*/
 static struct acc_hw cust_acc_hw = {
     .i2c_num = 1,
-    .direction = 6,
+    .direction = 5,
     .power_id = MT65XX_POWER_NONE,  /*!< LDO is not used */
     .power_vol= VOL_DEFAULT,        /*!< LDO is not used */
     .firlen = 16,                   /*!< don't enable low pass fileter */
